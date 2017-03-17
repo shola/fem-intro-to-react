9eaee73818513603773bad84854d23e39aad1507
shola <msitu@splunk.com>
2017-03-17 13:39:18 -0700

    start using webpack

--- js/ClientApp.es
+++ js/ClientApp.es
@@ -7,7 +7,7 @@ const div = React.DOM.div;
 // props are like parameters that are passed to a react element
 const MyFirstComponent = div(
     null,
-    React.createElement(MyTitle, { title: 'props work here too', color: 'red' }),
+    React.createElement(MyTitle, { title: 'props work here too!!!', color: 'red' }),
     React.createElement(MyTitle, { title: 'props are great', color: 'green' })
 );
 
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+    context: __dirname, // makes your app know where it lives on your file system
+    entry: './js/ClientApp',
+    output: {
+        path: path.join(__dirname, '/public'),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.json', '.es']
+    },
+    stats: {
+        colors: true,
+        reasons: true, // verbose error messages
+        chunks: false
+    },
+    module: {
+        // sequential processing utilities in our build pipeline
+        loaders: [
+            {
+                // if a file name passes the "test" regexp, run it through babel
+                test: /\.(js|jsx|es)?$/,
+                loader: 'babel-loader'
+            }
+        ]
+    }
+};
