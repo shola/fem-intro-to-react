6e3e830db092a2cf155aa2a7b9c33299ac0f64d9
shola <msitu@splunk.com>
2017-03-17 16:11:35 -0700

    Add Flow types to destructuring

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -2,20 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import MyTitle from './MyTitle';
 
-// TODO: remove for next lesson
-// props are like parameters that are passed to a react element
-//
-// this is my first stateless component! all it can do is render, no need
-// to use React.createElement until the life cycle management parts are needed
 const MyFirstComponent = () => (
     <div>
-        <MyTitle title="props work like this" color="mediumaquamarine" />
-        <MyTitle title="passing props to this component. nbd" color="rebeccapurple" />
+        <MyTitle title="props work like this!!!" color="mediumaquamarine" />
+        <MyTitle title="passing props to this component. nbd" />
     </div>
 );
 
 ReactDOM.render(<MyFirstComponent />, document.getElementById('app'));
-
-// TODO: remove for next lesson
-// These statesments are equivalent. Calling a stateless component's creator is the same as creating a jsx stateless component.
-// ReactDOM.render(MyFirstComponent(), document.getElementById('app'));
--- js/MyTitle.jsx
+++ js/MyTitle.jsx
@@ -2,19 +2,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// TODO: remove comment in next commit
 // Adding a type definition allows concise type annotation
-type MyTitleProps = {
-    color: string,
-    title: string
+type MyPropType = {
+    title: string,
+    color: string
 };
 
-// TODO: destructure "props"
-const MyTitle = (props: MyTitleProps) => {
-    const style = { color: props.color };
+// Here is how to inline a flow type. It's cumbersome, not reusable, and will be long
+// if you have more than a few props. Only here for illustrative purposes. Don't use this!
+//
+//      const MyTitle = ({ title, color = 'rebeccapurple' }: { title: string, color?: string }) => {
+const MyTitle = ({ title = 'Get Creative!', color = 'red' }: MyPropType) => {
+    const style = { color: color };
     return (
         <div>
             <h1 style={style}>
-                {props.title}
+                {title}
             </h1>
             <p>
                 {Date()}
